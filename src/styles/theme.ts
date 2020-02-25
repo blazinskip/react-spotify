50de51105e5432afb67347c77fc8679c85d0757b
pblazinski <pblazinski12013@gmail.com>
2020-02-25 19:31:39 +0100

    feat: 🎸 add styled component theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+  name: 'main',
+  borderRadius: '5px',
+
+  fontsFamilies: {
+    primary: 'sans-serif',
+  },
+  containerMaxWidth: '1200px',
+
+  colors: {
+    primary: 'black',
+  },
+};
