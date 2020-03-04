81c44786c17c4b2d59d76ecfd3ccf00688d46d82
blazinskip <blazinski.patryk@gmail.com>
2020-03-04 17:52:48 +0100

    feat: 🎸 add mock of libraries and playlist section in sidebar

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -9,9 +9,23 @@ export const theme: DefaultTheme = {
   },
   containerMaxWidth: '1200px',
 
+  spacing: {
+    xs: '8px',
+    s: '16px',
+    m: '32px',
+    l: '64px',
+  },
+
   colors: {
     white: '#fbfbfb',
     primary: '#1db954',
     primaryLight: '#1ed760',
+    greenDark: '#0c7b33',
+  },
+
+  links: {
+    color: 'greenDark',
+    hoverColor: 'primaryLight',
+    padding: 'xs',
   },
 };
