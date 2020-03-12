b370594f0ffa915722a1265d695221635a296326
blazinskip <blazinski.patryk@gmail.com>
2020-03-12 17:50:59 +0100

    feat: 🎸 add use playlists hooks

---
+++ src/hooks/usePlaylists.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Playlist } from '../models';
+
+export function usePlaylists(): [Playlist[]] {
+  const [playlists, setPlaylists] = useState<Playlist[]>([]);
+
+  useEffect(() => {
+    const getUserPlaylists = async () => {
+      const token = localStorage.getItem('token');
+      const tokenType = localStorage.getItem('tokenType');
+      if (token && tokenType) {
+        const { data } = await axios(' https://api.spotify.com/v1/me/playlists', {
+          headers: {
+            Authorization: `${tokenType} ${token}`,
+          },
+        });
+        setPlaylists(() => [...data.items]);
+      } else {
+        setPlaylists(() => []);
+      }
+    };
+    getUserPlaylists();
+  }, []);
+
+  return [playlists];
+}
