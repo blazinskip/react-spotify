c05f28070f6a56db9d90b40c52760c5c959a7bab
blazinskip <blazinski.patryk@gmail.com>
2020-03-19 17:21:57 +0100

    feat: 🎸 add play/resume in player bottom

--- src/components/SpotifyWrapper.tsx
+++ src/components/SpotifyWrapper.tsx
@@ -16,9 +16,21 @@ const SpotifyWrapper = ({ children }: PropsWithChildren<{}>) => {
     localStorage.setItem('tokenType', params.token_type);
   }
 
+  const [paused, setPaused] = useState(false);
   const [user] = useGetUserMe();
   const [playlists] = usePlaylists();
   const { player, deviceId } = useSpotifyPlayer();
+
+  player?.addListener('player_state_changed', () => {
+    player?.getCurrentState().then(state => {
+      if (!state) {
+        console.error('User is not playing music through the Web Playback SDK');
+        return;
+      }
+
+      setPaused(() => state.paused);
+    });
+  });
 
   const [context, setContext] = useState({
     token: localStorage.getItem('token'),
@@ -27,11 +39,12 @@ const SpotifyWrapper = ({ children }: PropsWithChildren<{}>) => {
     playlists,
     player,
     deviceId,
+    paused,
   });
 
   useEffect(() => {
-    setContext(state => ({ ...state, user, playlists, player, deviceId }));
-  }, [user, playlists, player, deviceId]);
+    setContext(state => ({ ...state, user, playlists, player, deviceId, paused }));
+  }, [user, playlists, player, deviceId, paused]);
 
   return <SpotifyClientContext.Provider value={context}>{children}</SpotifyClientContext.Provider>;
 };
