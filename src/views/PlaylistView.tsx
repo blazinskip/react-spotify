526b0308871011ebd9c4da57c9c2fb3202f0d60a
blazinskip <blazinski.patryk@gmail.com>
2020-03-22 12:09:00 +0100

    feat: 🎸 extract currentPlayedTrackId into playerState

--- src/views/PlaylistView.tsx
+++ src/views/PlaylistView.tsx
@@ -1,31 +1,26 @@
-import React, { FunctionComponent, useContext, useState } from 'react';
+import React, { FunctionComponent, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import { usePlaylistById } from '../hooks';
 import SpotifyClientContext from '../context/SpotifyClientContext';
-import { PlayerTrack } from '../models';
 import BaseLoading from '../components/BaseLoading';
 import PlaylistPage from '../components/PlaylistPage';
 
 const PlaylistView: FunctionComponent = () => {
   const { id } = useParams();
-  const [currentTrackId, setCurrentTrackId] = useState('');
   const { playlist } = usePlaylistById(id);
   const {
+    playerState: { currentPlayedTrackId },
     playerFunctions: { playUri, pausePlayer },
   } = useContext(SpotifyClientContext);
 
-  // player?.on('player_state_changed', ({ track_window: { current_track } }: Spotify.PlaybackState) => {
-  //   const {
-  //     id,
-  //     linked_from: { id: linkedFromId },
-  //   } = current_track as PlayerTrack;
-  //
-  //   setCurrentTrackId(() => linkedFromId ?? id);
-  // });
-
   if (playlist) {
     return (
-      <PlaylistPage playlist={playlist} currentTrackId={currentTrackId} playUri={playUri} pausePlayer={pausePlayer} />
+      <PlaylistPage
+        playlist={playlist}
+        currentTrackId={currentPlayedTrackId}
+        playUri={playUri}
+        pausePlayer={pausePlayer}
+      />
     );
   } else {
     return <BaseLoading />;
