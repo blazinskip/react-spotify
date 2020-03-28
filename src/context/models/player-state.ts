f7bbfde9c6b4eefae2a8789ecedb7677e931fa55
blazinskip <blazinski.patryk@gmail.com>
2020-03-28 11:18:10 +0100

    feat: 🎸 add current track to context

--- src/context/models/player-state.ts
+++ src/context/models/player-state.ts
@@ -1,4 +1,7 @@
+import { PlayerTrack } from '../../models';
+
 export interface PlayerState {
   readonly currentPlayedTrackId: string;
   readonly paused: boolean;
+  readonly currentTrack: PlayerTrack | {};
 }
